25dae534d576dcc6cdc118889c8a159179fd25ac
jorgechvz <cha22060@byui.edu>
2024-11-08 17:33:40 -0500

    refactor: Remove unused ServeStaticModule import from app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,6 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
