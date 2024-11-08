a547a59b509a57440ca0c73e2ad8793765e03b06
jorgechvz <cha22060@byui.edu>
2024-11-08 17:26:13 -0500

    feat: Update backend configuration for Vercel deployment and add static file serving

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,17 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), 
     }),
     AuthModule,
     UsersModule,
